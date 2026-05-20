a3599d8253bb4aabaf4fb89c5ba3d795899029d7
Tommy <57922367+tommy128works@users.noreply.github.com>
2026-05-20 19:10:41 -0400

    feat: implement RegisterPage

--- src/api/authApi.ts
+++ src/api/authApi.ts
@@ -1,5 +1,21 @@
 import { api, setAccessToken } from "./client";
 
+export const register = async (
+  firstName: string,
+  lastName: string,
+  email: string,
+  password: string,
+) => {
+  const res = await api.post("/auth/register", {
+    firstName,
+    lastName,
+    email,
+    password,
+  });
+
+  return res.data;
+};
+
 export const login = async (email: string, password: string) => {
   const res = await api.post("/auth/login", { email, password });
   setAccessToken(res.data.accessToken);
--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -1,8 +1,86 @@
+import { useState } from "react";
+import { register } from "../api/authApi";
+import { Link, useNavigate } from "react-router-dom";
+
 export default function RegisterPage() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e: React.SyntheticEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setError("");
+
+    if (!email || !password) {
+      setError("Email and password are required");
+      return;
+    }
+
+    if (password !== confirmPassword) {
+      setError("Passwords do not match.");
+      return;
+    }
+    try {
+      await register(firstName, lastName, email, password);
+      navigate("/");
+    } catch (err) {
+      setError("Registration failed.");
+    }
+  };
+
   return (
     <div style={{ textAlign: "center", marginTop: "50px" }}>
       <h1>Register Page</h1>
-      <p>Form will go here.</p>
+      <form onSubmit={handleSubmit}>
+        <h2>Create Account</h2>
+
+        {error && <p>{error}</p>}
+
+        <input
+          type="text"
+          placeholder="First Name"
+          value={firstName}
+          onChange={(e) => setFirstName(e.target.value)}
+        />
+
+        <input
+          type="text"
+          placeholder="Last Name"
+          value={lastName}
+          onChange={(e) => setLastName(e.target.value)}
+        />
+
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <input
+          type="password"
+          placeholder="Confirm Password"
+          value={confirmPassword}
+          onChange={(e) => setConfirmPassword(e.target.value)}
+        />
+
+        <button type="submit">{"Register"}</button>
+
+        <p style={{ marginTop: "10px" }}>
+          Already have an account? <Link to="/login">Login</Link>
+        </p>
+      </form>
     </div>
   );
 }
