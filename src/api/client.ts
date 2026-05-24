7169e88ea4c1e821be0a33ab8945191cfb1d924d
Tommy <57922367+tommy128works@users.noreply.github.com>
2026-05-24 17:15:04 -0400

    chore: add .env file and ignore it in .gitignore

--- src/api/client.ts
+++ src/api/client.ts
@@ -7,7 +7,7 @@ export const setAccessToken = (token: string | null) => {
 };
 
 export const api = axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
 });
 
